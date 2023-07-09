ae31776e0933f26921bf3233ea1d1978545a2ef7
dparrova <dfchaparrov@unal.edu.co>
2023-07-09 17:03:16 -0500

    uso de hbs

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const app = express()
+const port= 8080;
+
+app.set('view engine', 'hbs');
+
 
 // servir contenido estaticoo
 app.use(express.static('public'));
 
-app.get('/holamundo',  (req, res) => {
+app.get('/',  (req, res) => {
+  res.render('home');
+})
 
+app.get('/generic',  (req, res) => {
+  res.sendFile(__dirname+'/public/generic.html');
+})
+app.get('/elements',  (req, res) => {
+  res.sendFile(__dirname+'/public/elements.html');
 })
 
 app.get('*',  (req, res) => {
   res.sendFile(__dirname + '/public/404.html');
 })
 
-app.listen(8080)
+app.listen(port)
