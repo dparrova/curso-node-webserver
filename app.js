4e2d56252c0da2f3329addaff290fd6285bb1d24
dparrova <dfchaparrov@unal.edu.co>
2023-07-09 18:18:56 -0500

    agregando variables de entorno

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
+const hbs = require('hbs');
+require('dotenv').config()
+
 const express = require('express')
-const app = express()
-const port= 8080;
+const app = express();
+const port= process.env.PORT;
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 
 // servir contenido estaticoo
 app.use(express.static('public'));
+const params ={
+  nombre: 'Diego Chaparro',
+  titulo: 'Curso de node'
+}
 
 app.get('/',  (req, res) => {
-  res.render('home');
+  res.render('home',params);
 })
-
 app.get('/generic',  (req, res) => {
-  res.sendFile(__dirname+'/public/generic.html');
+ res.render('generic',params)
 })
 app.get('/elements',  (req, res) => {
-  res.sendFile(__dirname+'/public/elements.html');
+ res.render('elements',params)
 })
+
 
 app.get('*',  (req, res) => {
   res.sendFile(__dirname + '/public/404.html');
 })
 
-app.listen(port)
+app.listen(port,()=>{
+  console.log(`aplicación trabajando en el puestro: ${port}`)
+})
